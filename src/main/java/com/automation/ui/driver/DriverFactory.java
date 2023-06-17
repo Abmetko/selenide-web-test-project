4dfd2b18d2f3abcf2390aaf368a31c090747b511
abmetko.a <abmetko.a@gmail.com>
2023-06-17 17:48:32 +0300

    added firefox option

--- src/main/java/com/automation/ui/driver/DriverFactory.java
+++ src/main/java/com/automation/ui/driver/DriverFactory.java
@@ -4,8 +4,10 @@ import com.codeborne.selenide.Configuration;
 import com.codeborne.selenide.WebDriverRunner;
 import io.github.bonigarcia.wdm.WebDriverManager;
 import lombok.SneakyThrows;
+import lombok.Synchronized;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeOptions;
+import org.openqa.selenium.firefox.FirefoxOptions;
 import org.openqa.selenium.remote.RemoteWebDriver;
 
 import java.net.URL;
@@ -16,58 +18,62 @@ import java.util.Map;
 //mvn clean test -Dbrowser.type=chrome
 public class DriverFactory {
 
-    private static final ThreadLocal<WebDriver> driver = new ThreadLocal<WebDriver>();
+    private static final ThreadLocal<RemoteWebDriver> driver = new ThreadLocal<RemoteWebDriver>();
 
     /*
     it is not necessary to put and store WebDriver in ThreadLocal,
     in case if we use Selenide(it is already implemented out of the box there)
      */
+    @Synchronized
     public static void initDriver() {
-        switch (System.getProperty("browser.type")) {
-            case "firefox" -> WebDriverManager.firefoxdriver().setup();
-            case "chrome" -> WebDriverManager.chromedriver().setup();
-            default -> throw new IllegalArgumentException();
-        }
         driver.set(configureDriver());
-        WebDriverRunner.setWebDriver(driver.get());//to pass WebDriver to Selenide
+        //to pass WebDriver to Selenide
+        WebDriverRunner.setWebDriver(driver.get());
         Configuration.reportsFolder = "target/test-result/reports";
     }
 
     @SneakyThrows
     public static RemoteWebDriver configureDriver() {
-        WebDriverManager.chromedriver().setup();
-        ChromeOptions options = new ChromeOptions();
-        options.setCapability("selenoid:options", new HashMap<String, Object>() {{
-            /* How to add test badge */
+        RemoteWebDriver remoteWebDriver;
+        HashMap<String, Object> map = new HashMap<>() {{
             put("name", "SELENOID TEST");
-            /* How to set session timeout */
             put("sessionTimeout", "15m");
             put("-timeout", "60s");
             put("-limit", "10");
-            /* How to set timezone */
             put("env", new ArrayList<String>() {{
                 add("TZ=UTC");
             }});
-            /* How to add "trash" button */
             put("labels", new HashMap<String, Object>() {{
                 put("manual", "true");
             }});
-            /* How to enable video recording */
             put("enableVNC", true);
-//            put("enableVideo", true);
-        }});
-        Map<String, Object> prefs = new HashMap<>();
-        prefs.put("profile.default_content_setting_values.notifications", 1);
-//        prefs.put("intl.accept_languages", "ru,ru_RU");
-        options.addArguments("--disable-notifications");
-        options.addArguments("--disable-infobars");
-        options.setExperimentalOption("prefs", prefs);
-        options.setExperimentalOption("excludeSwitches", new String[]{"enable-automation"});
-//        options.setHeadless(false);
-        options.addArguments("–-no-sandbox");
-        options.addArguments("--incognito");
-        options.addArguments("--start-maximized");
-        return new RemoteWebDriver(new URL("http://localhost:4444/wd/hub"), options);
+        }};
+
+        switch (System.getProperty("browser.type")) {
+            case "firefox" -> {
+                WebDriverManager.firefoxdriver().setup();
+                FirefoxOptions options = new FirefoxOptions();
+                options.setCapability("selenoid:options", map);
+                remoteWebDriver = new RemoteWebDriver(new URL("http://localhost:4444/wd/hub"), options);
+            }
+            case "chrome" -> {
+                WebDriverManager.chromedriver().setup();
+                ChromeOptions options = new ChromeOptions();
+                options.setCapability("selenoid:options", map);
+                Map<String, Object> prefs = new HashMap<>();
+                prefs.put("profile.default_content_setting_values.notifications", 1);
+                options.addArguments("--disable-notifications");
+                options.addArguments("--disable-infobars");
+                options.setExperimentalOption("prefs", prefs);
+                options.setExperimentalOption("excludeSwitches", new String[]{"enable-automation"});
+                options.addArguments("–-no-sandbox");
+                options.addArguments("--incognito");
+                options.addArguments("--start-maximized");
+                remoteWebDriver = new RemoteWebDriver(new URL("http://localhost:4444/wd/hub"), options);
+            }
+            default -> throw new IllegalArgumentException("Unknown browser");
+        }
+        return remoteWebDriver;
     }
 
     public static WebDriver getDriver() {
@@ -76,6 +82,7 @@ public class DriverFactory {
 
     public static void closeDriver() {
         driver.get().close();
+        driver.get().quit();
         driver.remove();
     }
 }
